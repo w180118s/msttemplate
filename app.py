ff44cd071b6c91a4d91c5e670ccd8eb480d59b2a
w180118s <rahasiahidup0@gmail.com>
2021-11-17 08:37:27 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "mysecretket"
+
+@app.route('/')
+def index():
+    return redirect(url_for('dashboard'))
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('/dashboard.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
